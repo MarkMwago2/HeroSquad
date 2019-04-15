a5c388718e472399f2138984fd71389dda1c1e87
MarkMwago2 <markmwago@gmail.com>
2019-04-15 10:47:44 +0300

    pass all Instances test

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,29 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hero {
     private String mHeroName;
     private int mHeroAge;
     private String mSpecialPower;
     private String mWeakness;
+    private static List<Hero> heroInstances = new ArrayList<Hero>();
+    private int mId;
 
-    public Hero(String heroName, int heroAge, String specialPower, String weakness ) {
+    public Hero(String heroName, int heroAge, String specialPower, String weakness) {
         mHeroName = heroName;
         mHeroAge = heroAge;
         mSpecialPower = specialPower;
         mWeakness = weakness;
+        heroInstances.add(this);
     }
 
-    public String getheroName(){
-    return mHeroName;
+    public String getheroName() {
+        return mHeroName;
     }
 
-    public int getheroAge(){
-    return mHeroAge;
+    public int getheroAge() {
+        return mHeroAge;
     }
 
-    public String getspecialPower(){
-    return mSpecialPower;
+    public String getspecialPower() {
+        return mSpecialPower;
     }
 
-    public String getweakness(){
-    return mWeakness;
+    public String getweakness() {
+        return mWeakness;
+    }
+
+    public static List<Hero> getAll() {
+        return heroInstances;
     }
 }
